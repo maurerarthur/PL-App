68f623780bcbedcde817c3c4ba09abbd7fe84eb0
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2020-07-25 23:46:45 -0300

    Fixed ongoing match notification

--- src/matchNotification.js
+++ src/matchNotification.js
@@ -1,46 +1,41 @@
-import { AsyncStorage } from 'react-native';
 import axios from 'axios';
 import LocalNotification from '../src/notificationHandler.js';
 import { token } from '../auth.json';
 
 export default MatchNotification = async (id) => {
 
-    if(await AsyncStorage.getItem("game")) {
+    let matchData = {
+        homeTeam: null,
+        awayTeam: null,
+        homeTeamScore: null,
+        awayTeamScore: null
+    };
 
-        let matchData = {
-            homeTeam: null,
-            awayTeam: null,
-            homeTeamScore: null,
-            awayTeamScore: null
-        };
+    axios.get(`https://api.football-data.org/v2/teams/${id}/matches?status=FINISHED&limit=1`, { //IN_PLAY
 
-        axios.get(`https://api.football-data.org/v2/teams/${id}/matches?status=IN_PLAY&limit=1`, {
+        headers: {
+            "X-Auth-Token": token
+        }
 
-            headers: {
-                "X-Auth-Token": token
-            }
+    }).then(res => {
 
-        }).then(res => {
+        if(res.data.count > 0) {
 
-            if(res.data.count > 0) {
+            res.data.matches.forEach(match => {
+                matchData = {
+                    homeTeam: match.homeTeam.name,
+                    awayTeam: match.awayTeam.name,
+                    homeTeamScore: match.score.fullTime.homeTeam,
+                    awayTeamScore: match.score.fullTime.awayTeam
+                }
+            });
 
-                res.data.matches.forEach(match => {
-                    matchData = {
-                        homeTeam: match.homeTeam.name,
-                        awayTeam: match.awayTeam.name,
-                        homeTeamScore: match.score.fullTime.homeTeam,
-                        awayTeamScore: match.score.fullTime.awayTeam
-                    }
-                });
+            const matchResult = `${matchData.homeTeam} ${matchData.homeTeamScore} x ${matchData.awayTeamScore} ${matchData.awayTeam}`;
 
-                const matchResult = `${matchData.homeTeam} ${matchData.homeTeamScore} x ${matchData.awayTeamScore} ${matchData.awayTeam}`;
+            LocalNotification("Ongoing match", matchResult);
 
-                LocalNotification("Ongoing match", matchResult);
+        }
 
-            }
-
-        });
-
-    }
+    });
 
 }
